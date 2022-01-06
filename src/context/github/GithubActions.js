903a0b9ebe1c4c627c9724efb1981475f6139eae
KatherineRen <renshidie@gmail.com>
2022-01-06 11:39:35 -0500

    clean

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -25,34 +25,3 @@ export const getUserAndRepos = async (login) => {
   ])
   return { user: user.data, repos: repos.data }
 }
-
-// export const getUser = async (login) => {
-//   const response = await fetch(`${GITHUB_URL}/users/${login}`, {
-//     headers: {
-//       Authorization: `token ${GITHUB_TOKEN}`,
-//     },
-//   })
-
-//   if (response.status === 404) {
-//     window.location = '/notfound'
-//   } else {
-//     const data = await response.json()
-//     return data
-//   }
-// }
-
-// export const getUserRepos = async (login) => {
-//   const params = new URLSearchParams({
-//     sort: 'created',
-//     per_page: 10,
-//   })
-
-//   const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`, {
-//     headers: {
-//       Authorization: `token ${GITHUB_TOKEN}`,
-//     },
-//   })
-
-//   const data = await response.json()
-//   return data
-// }
